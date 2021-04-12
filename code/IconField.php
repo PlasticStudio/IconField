6e2953f65b010102cddbd8b26869f1591e3cdf91
psdigital <jeremy@psdigital.co.nz>
2021-04-12 14:48:18 +1200

    Update default path

--- code/IconField.php
+++ code/IconField.php
@@ -45,7 +45,7 @@ class IconField extends OptionsetField
     public function getFolderName()
     {
         if (is_null(self::$folder_name)) {
-            $this->folder_name = Config::inst()->get('PlasticStudio\IconField', 'icons_directory');
+            self::$folder_name = Config::inst()->get('PlasticStudio\IconField', 'icons_directory');
         }
         return self::$folder_name;
     }
